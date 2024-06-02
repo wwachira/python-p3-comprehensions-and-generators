56584cf04dd5edde00f781b981fbf35cbedd7393
lizwachira <lizwanguwachira@gmail.com>
2024-06-02 18:23:27 +0300

    list done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
+num_list = list(range(0, 100))
+print(return_evens(num_list))
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentences]
+
+sentences = ["Welcome", "To", "Moringa_School"]
+result = make_exclamation(sentences)
+print(result)
